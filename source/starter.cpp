3ed478476f6e5ca64ab0629c45ffd8e9261e9707
Kyrylo Kupin <95937194+kyrylokupin@users.noreply.github.com>
2024-07-04 16:16:05 +0300

    Add new project files with tests and updates .gitignore
    This commit adds a collection of initial files to kick-start a new project. It includes a base CMake configuration along with the main source file ('starter.cpp'), a couple of headers, and related tests. Moreover, the .gitignore file has been updated to leave out build files and IDE settings.

---
+++ source/starter.cpp
@@ -0,0 +1,9 @@
+#include "starter.hpp"
+
+#include <fmt/format.h>
+
+#include <iostream>
+
+auto tuposoft::starter::hello(std::string someone) -> void {
+    std::cout << fmt::format("Hello {}!", someone) << std::endl;
+}
---
+++ test/source/test.cpp
@@ -0,0 +1,9 @@
+#include "starter.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(starter, no_throws) {
+    ASSERT_NO_THROW({
+        tuposoft::starter::hello("world");
+    });
+}
